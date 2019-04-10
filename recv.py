cffe0f0b5c5083db7e074f7028fdd59ef09d0619
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2019-04-10 19:42:47 +1000

    Sort mailbox messages by contact and date

--- recv.py
+++ recv.py
@@ -8,25 +8,26 @@ config.read('.miabrc')
 M = imaplib.IMAP4_SSL(config['server']['hostname'])
 M.login(config['login']['user'], config['login']['password'])
 
-recv = []
+inbox = []
 M.select('INBOX', True)
-typ, data = M.uid('search', None, '(Header "Email2Chat-Version" "")')
+typ, data = M.uid('sort', '(FROM DATE)', 'UTF-8', 'ALL')
 for uid in data[0].split():
     typ, data = M.uid('fetch', uid, '(RFC822)')
     msg = email.message_from_bytes(data[0][1])
-    recv.append(msg)
-for msg in recv:
+    inbox.append(msg)
+for msg in inbox:
     print(msg)
+M.close()
 
 sent = []
 M.select('Sent', True)
-typ, data = M.uid('search', None, '(Header "Email2Chat-Version" "")')
+typ, data = M.uid('sort', '(TO DATE)', 'UTF-8', 'ALL')
 for uid in data[0].split():
     typ, data = M.uid('fetch', uid, '(RFC822)')
     msg = email.message_from_bytes(data[0][1])
     sent.append(msg)
 for msg in sent:
     print(msg)
-
 M.close()
+
 M.logout()
