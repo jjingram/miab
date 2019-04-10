80fd4bad77ac5e94e0c77619b906f53c5840fa7c
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2019-04-10 12:43:27 +1000

    Access the 'Sent' mailbox as well

--- recv.py
+++ recv.py
@@ -6,11 +6,20 @@ config.read('.miabrc')
 
 M = imaplib.IMAP4_SSL(config['server']['hostname'])
 M.login(config['login']['user'], config['login']['password'])
-M.select()
+
+M.select('INBOX')
 typ, data = M.search(None, 'ALL')
 for num in data[0].split():
     typ, data = M.fetch(num, '(RFC822)')
-    print('Message %s\n' % (num.decode('utf-8')))
+    print('Received Message %s\n' % (num.decode('utf-8')))
     print('%s\r\n' % (data[0][1].decode('utf-8')), end='\r\n')
+
+M.select('Sent')
+typ, data = M.search(None, 'ALL')
+for num in data[0].split():
+    typ, data = M.fetch(num, '(RFC822)')
+    print('Sent Message %s\n' % (num.decode('utf-8')))
+    print('%s\r\n' % (data[0][1].decode('utf-8')), end='\r\n')
+
 M.close()
 M.logout()
